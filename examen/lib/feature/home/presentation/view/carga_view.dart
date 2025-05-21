9ed787703cce5c1cde7ff9c33d17631de8c68b8c
AlfredUribe <alfreduriberolong20@gmail.com>
2025-05-21 11:19:35 -0500

    continuacion de las vistas (sin terminar)
    faltan las vistas succes y la error

--- examen/lib/feature/home/presentation/view/carga_view.dart
+++ examen/lib/feature/home/presentation/view/carga_view.dart
@@ -8,14 +8,45 @@ class Carga extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircularProgressIndicator(),
-            SizedBox(height: 20),
-            Text('cargando', style: TextStyle(fontSize: 18))
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade500, Colors.blue.shade100],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.hotel,
+                size: 70,
+                color: Colors.white,
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 200,
+                child: LinearProgressIndicator(
+                  backgroundColor: Colors.white24,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  minHeight: 6,
+                ),
+              ),
+
+              SizedBox(height: 20),
+              Text(
+                'cargando',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
